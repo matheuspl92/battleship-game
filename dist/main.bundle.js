cc45360957b314f3721716aeebe724e58bd3323c
Matheus <matheuspl92@hotmail.com>
2022-12-07 13:36:35 -0300

    Setup development environment

---
+++ dist/main.bundle.js
@@ -0,0 +1,26 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkbattleship_game"] = self["webpackChunkbattleship_game"] || []).push([["main"],{
+
+/***/ "./src/battleship.js":
+/*!***************************!*\
+  !*** ./src/battleship.js ***!
+  \***************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/battleship.js"));
+/******/ }
+]);
