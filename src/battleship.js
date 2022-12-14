fb912d601be2753199c00eb2c634b3b7c87c1b10
Matheus <matheuspl92@hotmail.com>
2022-12-14 15:42:51 -0300

    Initialize players and gameboards

---
+++ src/battleship.js
@@ -0,0 +1,26 @@
+const Player = require('./scripts/factories/player');
+const Gameboard = require('./scripts/factories/gameboard');
+
+const player1 = Player('Player');
+const player2 = Player('Computer');
+
+const shipsArray = [
+  {
+    name: 'Carrier', size: 5, x: 0, y: 0, orientation: 'vertical',
+  },
+  {
+    name: 'Battleship', size: 4, x: 2, y: 0, orientation: 'horizontal',
+  },
+  {
+    name: 'Cruiser', size: 3, x: 9, y: 0, orientation: 'vertical',
+  },
+  {
+    name: 'Submarine', size: 3, x: 0, y: 9, orientation: 'horizontal',
+  },
+  {
+    name: 'Destroyer', size: 2, x: 8, y: 9, orientation: 'horizontal',
+  },
+];
+
+const gameboard1 = Gameboard(shipsArray);
+const gameboard2 = Gameboard(shipsArray);
