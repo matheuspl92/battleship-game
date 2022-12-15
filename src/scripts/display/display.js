2fee3dbf50a88bd9bd35d377205d6783d524d1f4
Matheus <matheuspl92@hotmail.com>
2022-12-15 16:15:50 -0300

    Refactor HTML and display module

--- src/scripts/display/display.js
+++ src/scripts/display/display.js
@@ -1,6 +1,7 @@
 const display = () => {
   const container = document.getElementById('container');
-  const init = (player1, player2, gameboard1, gameboard2) => {
+
+  /* const init = (player1, player2, gameboard1, gameboard2) => {
     const messageBox = document.createElement('div');
     const player1Title = document.createElement('div');
     const player2Title = document.createElement('div');
@@ -11,10 +12,10 @@ const display = () => {
     player2Title.innerHTML = player2.name;
 
     container.append(messageBox, player1Title, player2Title, player1Gameboard, player2Gameboard);
-  };
+  }; */
 
   return {
-    init,
+    // init,
   };
 };
 
